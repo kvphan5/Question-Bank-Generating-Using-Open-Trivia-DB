06a7130b078c7d11a01c34fb211b2bfcce27a9c4
kvphan5 <kvphan5@gmail.com>
2021-05-16 20:19:16 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from data import question_data
+from question_model import Question
+from quiz_brain import QuizBrain
+
+#generate question bank from data
+question_bank = []
+for question in question_data:
+    new_question = Question(question["text"],question["answer"])
+    question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_question:
+    quiz.next_question()
